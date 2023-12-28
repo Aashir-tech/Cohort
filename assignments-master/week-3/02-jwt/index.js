2782dc7e49dbb162deb8854c88ee57594ce5985e
Aashir-tech <118300747+Aashir-tech@users.noreply.github.com>
2023-12-28 16:16:32 +0530

    JWT assignment solved and moved solution folder to week3

--- assignments-master/week-3/02-jwt/index.js
+++ assignments-master/week-3/02-jwt/index.js
@@ -1,16 +1,40 @@
 const jwt = require('jsonwebtoken');
+const zod = require('zod')
 const jwtPassword = "secret";
 
-function signJwt(username, password) {
+const emailSchema = zod.string().email()
+const passwordSchema = zod.string().min(6)
 
+function signJwt(username, password) {
+    const usernameResponse = emailSchema.safeParse(username);
+    const passwordResponse = passwordSchema.safeParse(password)
+
+    if(!usernameResponse.success || !passwordResponse.success) {
+        return null
+    }
+    const token = jwt.sign({
+        username
+    } , jwtPassword)
+    return token
 }
 
 function verifyJwt(token) {
-
+    try {
+        jwt.verify(token , jwtPassword);
+        return true
+    } catch (error) {
+        return false
+    }
+    
 }
 
 function decodeJwt(token) {
-
+    const decoded = jwt.decode(token);
+    if(decoded) {
+        return true
+    } else {
+        return false
+    }
 }
 
 module.exports = {
