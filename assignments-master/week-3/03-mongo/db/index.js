d77bdc5c2538a716d589ea5d9c794408e16c597f
Aashir-tech <118300747+Aashir-tech@users.noreply.github.com>
2024-01-06 12:40:45 +0530

    New folder created , file addition and solving assignments

--- assignments-master/week-3/03-mongo/db/index.js
+++ assignments-master/week-3/03-mongo/db/index.js
@@ -1,19 +1,32 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://aashirharis6:aashir7834@cluster.7paoqdf.mongodb.net/');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type :mongoose.Schema.Types.ObjectId,
+        ref : 'Course'
+    }] 
+
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    description : String, 
+    imageLink : String, 
+    price: Number
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
@@ -25,3 +38,6 @@ module.exports = {
     User,
     Course
 }
+
+//What is a Database
+//It is a place where data is stored persistently
--- assignments-master/week-3/03-mongo/routes/user.js
+++ assignments-master/week-3/03-mongo/routes/user.js
@@ -1,22 +1,85 @@
 const { Router } = require("express");
 const router = Router();
+const {User, Course} = require('../db');
 const userMiddleware = require("../middleware/user");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await User.create({
+        username,
+        password
+    })
+    res.json({
+        message: "User created successfully"
+    })
+
 });
 
 router.get('/courses', (req, res) => {
     // Implement listing all courses logic
+  Course.find({})
+  .then((response) => {
+    res.json({
+        courses : response
+    })
+  })
 });
 
 router.post('/courses/:courseId', userMiddleware, (req, res) => {
     // Implement course purchase logic
+    const courseId = req.params.courseId;
+    const username = req.headers.username;
+    User.updateOne({
+        username: username
+    }, {
+        "$push": {
+            purchasedCourses : courseId
+        }
+    })
+    .then(() => {
+        res.json({
+            message: "Purchase complete"
+        })
+    })
+    
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
     // Implement fetching purchased courses logic
+
+    // Using .then method 
+
+    /*
+    // User.findOne({
+    //     username: req.headers.username
+    // }).then((user) => {
+    //     return Course.find({
+    //         _id : {
+    //             "$in" : user.purchasedCourses
+    //         }
+    //     })
+    // }).then((courses) => {
+    //     res.json({
+    //         courses : courses
+    //     })
+    // })
+    */
+
+    const user = await User.findOne({
+        username: req.headers.username
+    });
+    const courses = await Course.find({
+        _id : {
+            "$in" : user.purchasedCourses
+        }
+    })
+    res.json({
+        courses : courses
+    })
 });
 
 module.exports = router
